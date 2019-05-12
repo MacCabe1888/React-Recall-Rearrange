323856042a8fc9947e28c17f5fc847f5b6d21966
Cameron <cameronjmaccabe@gmail.com>
2019-05-12 00:04:40 -0700

    added Bar component to be displayed at the top of the window

---
+++ src/components/Bar/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Message from "../Message";
+import "./style.css";
+
+function Bar(props) {
+  return (
+    <nav className="bar">
+      <ul>
+        <li className="logo">
+          <a href="/">React, Recall, Rearrange!</a>
+        </li>
+        <Message score={props.score} highScore={props.highScore} />
+        <li>
+          Score: {props.score} | High Score: {props.highScore}
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Bar;
