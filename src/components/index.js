8c5949c745875fac9e33c6e8c15d5742651ed27d
Cameron <cameronjmaccabe@gmail.com>
2019-05-12 00:09:59 -0700

    added index.js to centralize component exports for use in App.js

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Bar from "./Bar";
+import Clickable from "./Clickable";
+import Container from "./Container";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export {
+  Bar,
+  Clickable,
+  Container,
+  Footer,
+  Header
+};
