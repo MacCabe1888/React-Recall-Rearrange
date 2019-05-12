fb6c6752a123d4942562478ddf90518735a9789a
Cameron <cameronjmaccabe@gmail.com>
2019-05-12 00:07:53 -0700

    added Message component to handle conditional rendering of info displayed inside the Bar component

---
+++ src/components/Message/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Message extends Component {
+  state = {
+    guess: "",
+    animating: false
+  };
+
+  componentDidUpdate({ score, topScore }, prevState) {
+    const updatedState = { animating: true };
+
+    if (score === 0 && topScore === 0) {
+      updatedState.guess = "";
+    } else if (score === 0 && topScore > 0) {
+      updatedState.guess = "repeat";
+    } else {
+      updatedState.guess = "new";
+    }
+
+    if (score !== this.props.score || this.state.guess !== updatedState.guess) {
+      this.setState(updatedState);
+    }
+  }
+
+  renderText = () => {
+    switch (this.state.guess) {
+    case "new":
+      return "You chose wisely!";
+    case "repeat":
+      return "Uh oh! Repeat guess!";
+    default:
+      return "Click on an image to begin!";
+    }
+  };
+
+  render() {
+    return (
+      <li
+        className={this.state.animating ? this.state.guess : ""}
+        onAnimationEnd={() => this.setState({ animating: false })}
+      >
+        {this.renderText()}
+      </li>
+    );
+  }
+}
+
+export default Message;
