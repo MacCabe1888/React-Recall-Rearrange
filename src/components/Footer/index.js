506802fb001b16a0b286b308878c45cb158f51ef
Cameron <cameronjmaccabe@gmail.com>
2019-05-12 00:06:31 -0700

    added Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">
+        React, Recall, Rearrange! <img alt="react" src="assets/images/logo.svg" />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
