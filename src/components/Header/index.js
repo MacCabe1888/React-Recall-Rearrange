f453c6a5ba8fe7be3b9e5d0f5654c3cfb72bc0a5
Cameron <cameronjmaccabe@gmail.com>
2019-05-13 19:49:28 -0700

    edited header text

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <h1>React, Recall, Rearrange!</h1>
-      <h2>Click on an image to earn points, but don't click on any image more than once!</h2>
+      <h2>Click on an image to earn a point, but don't click on any image more than once in the same round!</h2>
     </header>
   );
 }
